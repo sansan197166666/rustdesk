0606acd38310b531122df648bbc815c6752c836f
sansan197166666 <sansan197166666@gmail.com>
2024-10-24 00:25:30 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["gn001.guansheng9922.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "Gji3wRpyeWWOr6AOPn1jI92TWx2389B49wXJ762Vhu8=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["156.251.24.60"];
+pub const PUBLIC_RS_PUB_KEY: &str = "UsocCNshBpILmAKkv3cbehRto9QPtgZ4pV8peWwScVo=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
